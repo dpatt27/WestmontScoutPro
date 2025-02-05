9809b4f60f17e65aa03461aa54c0be7ac3d149df
Daniel Patterson <danielpatterson@Daniels-MacBook-Air.local>
2025-02-05 13:57:34 -0800

    File upload with Models bug

--- heatmaps/models.py
+++ heatmaps/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 class Pitch(models.Model):
     pitcher = models.CharField(max_length=100)
-    platelocheight = models.FloatField(null=True, blank=True)
-    platelocside = models.FloatField(null=True, blank=True)
+    plate_loc_height = models.FloatField()
+    plate_loc_side = models.FloatField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.plate_loc_height} {self.plate_loc_side}"
--- heatmaps/views.py
+++ heatmaps/views.py
@@ -1,27 +1,3 @@
+from django.shortcuts import render
 
-import csv
-from django.shortcuts import render, redirect
-from .forms import CSVUploadForm
-from .models import Pitch
-
-
-def home(request):
-    return render(request, 'heatmaps/home.html')
-
-def upload_csv(request):
-    if request.method == 'POST':
-        form = CSVUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            csv_file = request.FILES['file']
-            decoded_file = csv_file.read().decode('utf-8').splitlines()
-            reader = csv.DictReader(decoded_file)
-            for row in reader:
-                Pitch.objects.create(
-                    pitcher=row['pitcher'],
-                    platelocheight=row.get('platelocheight') or None,
-                    platelocside=row.get('platelocside') or None
-                )
-            return redirect('success')
-    else:
-        form = CSVUploadForm()
-    return render(request, 'heatmaps/upload_csv.html', {'form': form})
+# Create your views here.
