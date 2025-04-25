742417b5aa04509637fd8c811fe7bf085134deaa
Daniel Patterson <danielpatterson@Daniels-MacBook-Air.local>
2025-04-24 22:21:23 -0700

    final commit

--- westmont_scout_pro/settings.py
+++ westmont_scout_pro/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 import dj_database_url
+import logging
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,6 +131,21 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
