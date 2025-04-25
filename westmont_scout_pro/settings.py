494d6eb9f730b84848515a57c8656c4a1cfe0a5f
Daniel Patterson <danielpatterson@Daniels-MacBook-Air.local>
2025-04-24 22:02:09 -0700

    Commit for heroku

--- westmont_scout_pro/settings.py
+++ westmont_scout_pro/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*hspc(m#2+$8-una8m*pihk)$$m2a&)b)o2&la@(x&+emwg!_i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = True
 
-ALLOWED_HOSTS = ['westmontscoutpro-caed3c76dcc9.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
