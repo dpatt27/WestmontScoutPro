2231f724a58336f8be28d02f59d5f05727b3f80e
Daniel Patterson <danielpatterson@Daniels-MacBook-Air.local>
2025-04-26 23:30:56 -0700

    changes csvs views for heroku

--- csvs/views.py
+++ csvs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CsvModelForm
 from .models import Csv
 import csv
@@ -9,31 +9,38 @@ import pandas as pd
 def upload_file_view(request):
     form = CsvModelForm(request.POST or None, request.FILES or None)
     if form.is_valid():
-        form.save()
-        form = CsvModelForm()
-        obj = Csv.objects.filter(activated=False).first()  # Updated this line
-        if obj:  # Check if obj is not None
-            df = pd.read_csv(obj.file_name.path)
-            columns_to_keep = ['Pitcher', 'TaggedPitchType', 'RelSpeed', 'PlateLocHeight', 'PlateLocSide', 'PitchCall', 'ExitSpeed', 'Batter', 'BatterTeam', 'PlayResult', 'KorBB']
-            new_df = df[columns_to_keep]
-            new_df['ExitSpeed'] = new_df['ExitSpeed'].fillna(0)
-            new_df = new_df.dropna()
-            print(new_df)
-            for index, row in new_df.iterrows():
-                pitch_instance = Pitch(
-                    pitcher=row['Pitcher'],
-                    pitchtype=row['TaggedPitchType'],
-                    velo=row['RelSpeed'],
-                    platelocheight=row['PlateLocHeight'],
-                    platelocside=row['PlateLocSide'],
-                    pitchcall=row['PitchCall'],
-                    exitspeed=row['ExitSpeed'],
-                    batter=row['Batter'],
-                    batter_team=row['BatterTeam'],
-                    playresult=row['PlayResult'],
-                    korbb = row['KorBB'],
-                )
-                pitch_instance.save()
-            obj.activated = True
-            obj.save()
+        form_instance = form.save(commit=False)  # Save but don't write file to disk
+        csv_file = request.FILES['file']
+
+        df = pd.read_csv(csv_file)
+
+        columns_to_keep = [
+            'Pitcher', 'TaggedPitchType', 'RelSpeed', 'PlateLocHeight',
+            'PlateLocSide', 'PitchCall', 'ExitSpeed', 'Batter',
+            'BatterTeam', 'PlayResult', 'KorBB'
+        ]
+        new_df = df[columns_to_keep]
+        new_df['ExitSpeed'] = new_df['ExitSpeed'].fillna(0)
+        new_df = new_df.dropna()
+
+        for _, row in new_df.iterrows():
+            Pitch.objects.create(
+                pitcher=row['Pitcher'],
+                pitchtype=row['TaggedPitchType'],
+                velo=row['RelSpeed'],
+                platelocheight=row['PlateLocHeight'],
+                platelocside=row['PlateLocSide'],
+                pitchcall=row['PitchCall'],
+                exitspeed=row['ExitSpeed'],
+                batter=row['Batter'],
+                batter_team=row['BatterTeam'],
+                playresult=row['PlayResult'],
+                korbb=row['KorBB'],
+            )
+
+        form_instance.activated = True
+        form_instance.save()
+
+        return redirect('home')  # or wherever you want to redirect
+
     return render(request, 'csvs/upload.html', {'form': form})
